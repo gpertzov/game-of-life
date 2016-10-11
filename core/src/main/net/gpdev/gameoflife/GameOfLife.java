8845b6740ce046288b58d4993bdd94641ca0aef9
gpertzov <gamedev@gpdev.net>
2016-10-11 15:15:48 +0300

    Populate empty cell if it has 3 neighbors (make failing test pass)

--- core/src/main/net/gpdev/gameoflife/GameOfLife.java
+++ core/src/main/net/gpdev/gameoflife/GameOfLife.java
@@ -1,10 +1,15 @@
 package net.gpdev.gameoflife;
 
 public class GameOfLife {
+    private final int xDim;
+    private final int yDim;
+
     private Grid current;
     private Grid next;
 
     public GameOfLife(int xDim, int yDim) {
+        this.xDim = xDim;
+        this.yDim = yDim;
         current = new Grid(xDim, yDim);
         next = new Grid(xDim, yDim);
     }
@@ -18,6 +23,15 @@ public class GameOfLife {
     }
 
     public void tick() {
+        for (int i = 0; i < xDim; i++) {
+            for (int j = 0; j < yDim; j++) {
+                if (current.isEmpty(i, j)) {
+                    if (current.numNeighbors(i, j) == 3) {
+                        next.set(i, j);
+                    }
+                }
+            }
+        }
         current = next;
     }
 }
