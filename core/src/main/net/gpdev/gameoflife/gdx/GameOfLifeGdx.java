80f273a6b37699bf88237fca0b2525d434e14a66
gpertzov <gamedev@gpdev.net>
2016-12-23 14:29:12 +0200

    Fix screen dispose not being called

--- core/src/main/net/gpdev/gameoflife/gdx/GameOfLifeGdx.java
+++ core/src/main/net/gpdev/gameoflife/gdx/GameOfLifeGdx.java
@@ -1,12 +1,20 @@
 package net.gpdev.gameoflife.gdx;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Screen;
 
 public class GameOfLifeGdx extends Game {
 
+    private final Screen gameScreen = new GameScreen();
 
     @Override
     public void create() {
-        setScreen(new GameScreen());
+        setScreen(gameScreen);
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        gameScreen.dispose();
     }
 }
